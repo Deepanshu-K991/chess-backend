6086e6285777c6abefa8cf6d8d296fb4962415fd
Deepanshu-K991 <deepanshukumar842@gmail.com>
2025-08-05 01:29:38 +0530

    writeing moves

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const socket  = require("socket.io");
 const http = require("http");
 const {Chess} = require("chess.js");
 const path = require("path"); 
+const { scheduler } = require("timers/promises");
 
 const app = express();
 
@@ -28,8 +29,39 @@ io.on("connection" , function(uniquesocket){
     //     io.emit("test data 1");
     // })
     
+    if(!players.white){
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole" , "w");
+    }
+    else if(!players.black){
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole" , "b");
+    }
+    else{
+        uniquesocket.emit("spectatorRole");
+    }
+    socket.on("disconnect" , function(){
+        if(uniquesocket.id === players.white){
+            delete players.white
+        }
+        else if(uniquesocket.id === players.black){
+            delete players.black
+        }
+    });
+    uniquesocket.on("move" , (move) => {
+        try {
+            if(chess.turn()=== "w" && socket.id!= players.white) return;
+            if(chess.turn()=== "b" && socket.id!= players.black) return;
 
-
+           const result =  chess.move(move);
+           if(result){
+            currentPlayer = chess.turn();
+            io.emit("move" , move);
+           }
+        } catch (error) {
+            
+        }
+    })
 
 });
 
